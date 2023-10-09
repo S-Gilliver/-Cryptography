396c913bd740664a5788c39060f416ca85149de4
S-Gilliver <shtyrkov.dmitry@yandex.ru>
2023-10-09 23:03:13 +0300

    PZ-3

---
+++ PZ-3/Calculator.java
@@ -0,0 +1,39 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите числа:");
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+
+        while (true) {
+            System.out.println("Введите операцию (1 - сложение) (2 - вычитание) (3 - умножение) (4 - деление) (0 - Выход)");
+            String oper = scanner.next();
+
+            if (oper.equals("1")) {
+                System.out.println("Результат: " + (BigDecimal.valueOf(num1)
+                        .add(BigDecimal.valueOf(num2))));
+            } else if (oper.equals("2")) {
+                System.out.println("Результат: " + (BigDecimal.valueOf(num1)
+                        .subtract(BigDecimal.valueOf(num2))));
+            } else if (oper.equals("3")) {
+                    System.out.println("Результат: " + (new BigDecimal(num1)
+                            .multiply(new BigDecimal(num2))));
+            } else if (oper.equals("4")) {
+                if (num2 != 0) {
+                    System.out.println("Результат: " + (new BigDecimal(num1)
+                            .divide(new BigDecimal(num2))));
+                } else {
+                    System.out.println("Деление на ноль невозможно.");
+                }
+            } else if (oper.equals("0")) {
+                break;
+            } else {
+                System.out.println("Такой операции не существует");
+            }
+        }
+        scanner.close();
+    }
+}
