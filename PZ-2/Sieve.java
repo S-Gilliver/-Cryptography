b16ba0f21441188b16334dad3cb6f8d5eb95b673
S-Gilliver <shtyrkov.dmitry@yandex.ru>
2023-10-09 23:53:12 +0300

    PZ-2

--- PZ-2/Sieve.java
+++ PZ-2/Sieve.java
@@ -1,16 +1,23 @@
 import java.util.Scanner;
 
 public class Sieve {
+    // Функция для решета Эратосфена, возвращает массив булевых значений, где каждый индекс представляет число, а значение указывает,
+    // является ли это число простым (true) или составным (false).
     public static boolean[] sieveEratosthenes(int n) {
         boolean[] isPrime = new boolean[n + 1];
+
+        // Инициализируем массив как все числа простыми
         for (int i = 0; i <= n; i++) {
             isPrime[i] = true;
         }
+
+        // 0 и 1 не являются простыми числами
         isPrime[0] = isPrime[1] = false;
 
         int p = 2;
         while (p * p <= n) {
             if (isPrime[p]) {
+                // Помечаем все кратные p как составные числа
                 for (int i = p * p; i <= n; i += p) {
                     isPrime[i] = false;
                 }
@@ -21,6 +28,7 @@ public class Sieve {
         return isPrime;
     }
 
+    // Функция для проверки, является ли число простым, используя решето Эратосфена
     public static boolean isPrimeUsingSieve(int n) {
         boolean[] primes = sieveEratosthenes(n);
         return primes[n];
